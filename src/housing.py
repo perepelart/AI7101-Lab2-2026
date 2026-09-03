cc792bbb890406912b3dd585f51f608de308d513
Artem Perepelitsyn <perepelart@yandex.com>
2026-09-03 13:29:31 +0400

    Understood that the problem wasn't in fixing the errors and handeled the conflict by reusing the already defined variable for the newly added regression to ensure reproducibility.

--- src/housing.py
+++ src/housing.py
@@ -18,21 +18,21 @@ BASE_PIPELINE = [
 ]
 
 LINEAR_GRID = {
-    "model__alpha_linear": [0.001, 0.01, 0.1, 1.0, 10.0],
+    "model__alpha_linear": [0.001, 0.01, 0.1, 0.3, 0.5, 0.7, 1.0, 10.0],
     "model__l1_ratio": [0.0, 0.5, 1.0],
 }
 
-SECOND_DEGREE_GRID = {
-    "model__alpha_second_degree": [0.001, 0.01, 0.1, 1.0, 10.0],
-    "model__l1_ratio": [0.0, 0.5, 1.0],
+# SECOND_DEGREE_GRID = {
+#     "model__alpha_second_degree": [0.001, 0.01, 0.1, 1.0, 10.0],
+#     "model__l1_ratio": [0.0, 0.5, 1.0],
 
-}
+# }
 
-THIRD_DEGREE_GRID = {
-    "model__alpha_third_degree": [0.001, 0.01, 0.1, 0.3, 0.5, 0.7, 1.0, 10.0],
-    "model__l1_ratio": [0.0, 0.5, 1.0],
+# THIRD_DEGREE_GRID = {
+#     "model__alpha_third_degree": [0.001, 0.01, 0.1, 0.3, 0.5, 0.7, 1.0, 10.0],
+#     "model__l1_ratio": [0.0, 0.5, 1.0],
 
-}
+# }
 
 MODELS = {
     "simple_elastic": {
@@ -47,7 +47,7 @@ MODELS = {
                     ("model", ElasticNet(max_iter=1000)),
                 ]
             ),
-            "param_grid": SECOND_DEGREE_GRID,
+            "param_grid": LINEAR_GRID,
         },
     "poly_elastic_3": {
         "pipeline": Pipeline(
@@ -57,7 +57,7 @@ MODELS = {
                 ("model", ElasticNet(max_iter=1000)),
             ]
         ),
-        "param_grid": THIRD_DEGREE_GRID,
+        "param_grid": LINEAR_GRID,
     },
     "knn": {
         "pipeline": Pipeline(BASE_PIPELINE + [("model", KNeighborsRegressor())]),
